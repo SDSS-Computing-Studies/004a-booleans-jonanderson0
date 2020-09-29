b6024f34ac8917c4fccf00bca3fe48c0d4f59e66
jonanderson0 <71406833+jonanderson0@users.noreply.github.com>
2020-09-29 12:25:55 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -5,4 +5,13 @@
 # but if it is, then ask them for a password 
 # If they get the password correct (password is 12345password) 
 # then display the message "Access granted"
-# 1 marks
+# 1 mark
+username= input("enter a username")
+
+if "admin" in username:
+  print("Enter password")
+else:
+  print("invalid user")
+if "12345password" in password:
+  print("Access granted")
+
