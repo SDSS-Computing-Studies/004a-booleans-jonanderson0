6cf28feb58c78e8e8000b9866ead7e3e1f19e03f
jonanderson0 <71406833+jonanderson0@users.noreply.github.com>
2020-10-01 19:00:16 -0700

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -12,5 +12,35 @@
 # - "that is a right triangle"
 # - "that is an acute triangle"
 # - "that is an obtuse triangle"
-x = ("that is a right triangle")
-print(x)
+ import math
+  
+a = float(input("enter first side; "))
+b = float(input("enter second side; "))
+c = float(input("enter third side; "))
+
+if a > b and a > c:
+  hyp = a
+elif b > a and b > c:
+  hyp = b
+elif c > a and c > b:
+  hyp = c
+  
+if hyp == a:
+  side1 = b
+  side2 = c
+elif hyp == b:
+  side1 = a
+  side2 = c
+elif hyp == c:
+  side1= a
+  side2= b
+
+actual_hyp = side1**2 + side2**2
+
+if side1**2 + side2**2 == round(hyp)**2 and (hyp - actual_hyp > 0.02*hyp or hyp - actual_hyp < -0.02*hyp):
+  print("that is a right triangle")
+elif side1**2 + side2**2 > hyp**2:
+  print("that is an acute triangle")
+elif side1**2 + side2**2 < hyp**2:
+  print("that is an obtuse triangle")
+
